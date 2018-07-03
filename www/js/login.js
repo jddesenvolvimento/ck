7cac15e2267c122be4d1635b16cd0b665ddb654b
%jadsondia% <%jadsondia@hotmail.com%>
2018-07-03 14:12:43 -0300

    commit 1

---
+++ www/js/login.js
@@ -0,0 +1,25 @@
+var urlApp = "http://icmpe.com.br/appcheckin/";
+
+function login() { //mostra detalhes dos eventos que serão importados
+    $.ajax({
+        type: "GET",
+        url: urlApp+"login.php",
+        data: {
+            login: $('#login').val(),
+            senha: $('#senha').val()
+        },
+        success: function(data) {
+            if(data != ""){
+                
+                window.location = "pg.html";
+                
+            }else{
+                var erro = "Login ou Senha Incorretos!";
+                $('#retorno').html(erro);
+            }
+            
+        }
+    });
+}
+
+
