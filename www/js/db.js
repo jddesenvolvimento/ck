162c858af9b87868adb7d826d425cd3e14b3f3a4
%jadsondia% <%jadsondia@hotmail.com%>
2018-07-08 01:16:42 -0300

    commit 9

--- www/js/db.js
+++ www/js/db.js
@@ -1,16 +1,17 @@
 function createDB(){
     try{
-        //db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
+       // db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
         db = window.sqlitePlugin.openDatabase({name: 'checkinDB.db', location: 'default'}, function(db) {
             db.transaction(function(tx) {
                 tx.executeSql('CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY ASC, login VARCHAR(30), senha VARCHAR(50), nivel INT(1))');  
                 tx.executeSql('CREATE TABLE IF NOT EXISTS visitantes (id INTEGER PRIMARY KEY ASC, cpf INT(11), nome VARCHAR(50), igreja VARCHAR(30), areaIgreja VARCHAR(30), cBarras VARCHAR(10), tipoInscricao INT(1), nomeEquipe VARCHAR(30), funcao INT(1), presenca INT(1), dataEntrada VARCHAR(10), evento VARCHAR(30))');
             }, function(err) {
               console.log('Open database ERROR: ' + JSON.stringify(err));
+              alert("DB nao conectado");
             });
           });
     }catch (erro){
-        alert('Erro: ' + erro);
+        alert('Erro n conetado: ' + erro);
     }  
     console.log('Banco de Dados criado/aberto');
 }
@@ -43,7 +44,7 @@ function saveUser(id,login,senha,nivel){
             tx.executeSql('INSERT INTO user (id, login, senha, nivel) VALUES (?,?,?,?)',[id,login,senha,nivel]);
         })
     }catch(erro){
-       alert('Erro: ' + erro); 
+       alert('Nao Salvou Erro: ' + erro); 
     }
     console.log('usuario inserido');
 }
