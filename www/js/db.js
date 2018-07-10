b4ccf5238be8717906d83f6cf77d7f383ba48550
%jadsondia% <%jadsondia@hotmail.com%>
2018-07-10 14:17:52 -0300

    commit 11

--- www/js/db.js
+++ www/js/db.js
@@ -1,8 +1,8 @@
 function createDB(){
     try{
-        db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
-        //document.addEventListener('deviceready', function() {
-        //db = window.sqlitePlugin.openDatabase({name: 'checkinDB.db', location: 'default'}, function(db) {
+        //db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
+        document.addEventListener('deviceready', function() {
+        db = window.sqlitePlugin.openDatabase({name: 'checkinDB.db', location: 'default'}, function(db) {
             db.transaction(function(tx) {
                 tx.executeSql('CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY ASC, login VARCHAR(30), senha VARCHAR(50), nivel INT(1))');  
                 tx.executeSql('CREATE TABLE IF NOT EXISTS visitantes (id INTEGER PRIMARY KEY ASC, cpf INT(11), nome VARCHAR(50), igreja VARCHAR(30), areaIgreja VARCHAR(30), cBarras VARCHAR(10), tipoInscricao INT(1), nomeEquipe VARCHAR(30), funcao INT(1), presenca INT(1), dataEntrada VARCHAR(10), evento VARCHAR(30))');
@@ -10,8 +10,8 @@ function createDB(){
               console.log('Open database ERROR: ' + JSON.stringify(err));
               alert("DB nao conectado");
             });
-        //  });
-      //  });  
+          });
+        });  
     }catch (erro){
         alert('Erro n conetado: ' + erro);
     }  
